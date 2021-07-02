6f40f89a9175310494b883f31a1c4bca726e0b29
Felipe Ferreira <61047570+fellipefee@users.noreply.github.com>
2021-07-01 21:42:29 -0300

    Add files via upload
    teste automatizado utilizando selenium webdriver, Java, Junit, Page Objects e DSL.

---
+++ CursoSelenium/src/main/java/TesteCadastro.java
@@ -0,0 +1,90 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TesteCadastro {
+	
+	private WebDriver driver;
+	private DSL dsl;
+	private CampoTreinamentoPage page;
+
+	@Before
+	public void inicializa(){
+		driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(1200, 765));
+		driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+		dsl = new DSL(driver);
+		page = new CampoTreinamentoPage(driver);
+	}
+	
+	@After
+	public void finaliza(){
+		driver.quit();
+	}
+
+	@Test
+	public void deveRealizarCadastroComSucesso(){
+		page.setNome("Wagner");
+		page.setSobrenome("Costa");
+		page.setSexoMasculino();
+		page.setComidaPizza();
+		page.setEscolaridade("Mestrado");
+		page.setEsporte("Natacao");
+		page.cadastrar();
+		
+		Assert.assertTrue(page.obterResultadoCadastro().startsWith("Cadastrado!"));
+		Assert.assertTrue(page.obterNomeCadastro().endsWith("Wagner"));
+		Assert.assertEquals("Sobrenome: Costa", page.obterSobrenomeCadastro());
+		Assert.assertEquals("Sexo: Masculino", page.obterSexoCadastro());
+		Assert.assertEquals("Comida: Pizza", page.obterComidaCadastro());
+		Assert.assertEquals("Escolaridade: mestrado", page.obterEscolaridadeCadastro());
+		Assert.assertEquals("Esportes: Natacao", page.obterEsportesCadastro());
+	}
+	
+	@Test
+	public void deveValidarNomeObrigatorio(){
+		page.cadastrar();
+		Assert.assertEquals("Nome eh obrigatorio", dsl.alertaObterTextoEAceita());
+	}
+	
+	@Test
+	public void deveValidarSobrenomeObrigatorio(){
+		page.setNome("Nome qualquer");
+		page.cadastrar();
+		Assert.assertEquals("Sobrenome eh obrigatorio", dsl.alertaObterTextoEAceita());
+	}
+	
+	@Test
+	public void deveValidarSexoObrigatorio(){
+		page.setNome("Nome qualquer");
+		page.setSobrenome("Sobrenome qualquer");
+		page.cadastrar();
+		Assert.assertEquals("Sexo eh obrigatorio", dsl.alertaObterTextoEAceita());
+	}
+	
+	@Test
+	public void deveValidarComidaVegetariana(){
+		page.setNome("Nome qualquer");
+		page.setSobrenome("Sobrenome qualquer");
+		page.setSexoFeminino();
+		page.setComidaCarne();
+		page.setComidaVegetariano();
+		page.cadastrar();
+		Assert.assertEquals("Tem certeza que voce eh vegetariano?", dsl.alertaObterTextoEAceita());
+	}
+	
+	@Test
+	public void deveValidarEsportistaIndeciso(){
+		page.setNome("Nome qualquer");
+		page.setSobrenome("Sobrenome qualquer");
+		page.setSexoFeminino();
+		page.setComidaCarne();
+		page.setEsporte("Karate", "O que eh esporte?");
+		page.cadastrar();
+		Assert.assertEquals("Voce faz esporte ou nao?", dsl.alertaObterTextoEAceita());
+	}
+}
